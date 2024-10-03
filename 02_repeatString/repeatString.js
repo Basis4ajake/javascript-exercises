adacd89bf0dda1f48e91fbfe4e9a06bb5a290bc3
Jacob Marlowe <jacob.marlowe1991@gmail.com>
2024-10-03 00:04:26 -0400

    Complete exercises 1 and 2 for Test Driven Development

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+   if (num < 0) return "ERROR";
+   let word = "";
+      for (let i = 0; i < num; i++ ) {
+         word += string;
+      }
+   return word;
+   }
 
-};
+
+
+// console.log(repeatString("hey", 3))
+// console.log(repeatString("hello", 10))
+// console.log(repeatString('hi', 1))
+// console.log(repeatString('bye', 0))
+console.log(repeatString('goodbye', -1))
+// console.log(repeatString('', 5))
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
