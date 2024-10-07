dd255c8de5e23156b48cbb88fd1180c7621b5c59
Jacob Marlowe <jacob.marlowe1991@gmail.com>
2024-10-07 00:08:55 -0400

    Get sum of two numbers and any numbers between them

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sumOfNum1AndNum2 = num1 + num2;
+    if (num2 < num1) {
+        let i = num2;
+        } else {
+        i = num1;
+        }; 
+        while (i !== num1 || i !== num2) {
+        return totalResult = sumOfNum1AndNum2 += i
+        }
+        i++
+    }
 
-};
+totalResult = sumAll
+
+Implement a function that takes 2 positive integers and returns the sum of every integer between (and including) them:
+
+
+
+//Initialize counter based on which number is lower
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
