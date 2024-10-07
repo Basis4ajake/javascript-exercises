e9c9274d7f35e956bbb60123792a432349edc7d0
Jacob Marlowe <jacob.marlowe1991@gmail.com>
2024-10-06 23:11:32 -0400

    Pass all tests for exercise 4
    Tested using loop over rest parameters
    to get the index of items to remove,
    then use splice to remove the item from the array
    and mutate the original array. Was able to pass
    test one with the function using those methods but
    could not pass test 2. Had ideas for conditionals
    to use for some of the later tests, but did not
    move on from
    test 2.
    
    Researched other methods to utilize such as
    find(), map(), and join(). Read up on use cases of
    each before trying to use sort to create a new
    array that does not include the items to be
    removed.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,29 @@
-const removeFromArray = function() {
-};
+// const removeFromArray = function (myArray, ...itemsToRemove) {
+//   for (const item of itemsToRemove) {
+//     index = myArray.indexOf(item)
+//     myArray.splice(index, 1)
+//     return myArray;
+//   }
+//   }
+
+const removeFromArray = function (myArray, ... itemsToRemove) {
+  filteredArray = myArray.filter(item => !itemsToRemove.includes(item))
+  return filteredArray; 
+}
+
+myList = [1, 2, 3, 4];
+mySecondList= [1, 2, 3, 4];
+
+removeFromArray(myList, 3)
+removeFromArray(myList, 2, 3)
+removeFromArray([1, 2, 2, 4], 2)
+removeFromArray([1, 2, 3, 4], 7, "tacos")
+removeFromArray([1, 2, 3, 4], 3, 4, 5, 6)
+removeFromArray(myList, 1, 2, 3, 4)
+removeFromArray(["hey", 2, 3, "ho"], "hey", 2)
+removeFromArray([1, 2, 3, 4], "1", 2, 3)
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
