7f09b477d36168d2d7102784241282299c4da64f
Jacob Marlowe <jacob.marlowe1991@gmail.com>
2024-10-07 17:00:53 -0400

    Check if year can be divided by 4 and 100 but not 400, if so not a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
 const leapYears = function(year) {
-    if (year % 4 === 0) && (year % 400 === 0) && (year % 100 != 0) {
+    if (year % 4 != 0) {
+        let isLeapYear = false;
+        return isLeapYear;
+    } else if ((year % 4 === 0) && (year % 100 === 0) && (year % 400 !== 0)) {
+        let isLeapYear = false;
+        return isLeapYear;
+    } else {
+        let isLeapYear = true;
+        return isLeapYear;
+    }
+    
+    (year % 4 === 0) && (year % 400 === 0) && (year % 100 != 0) {
         return console.log(true);
     }
 };
