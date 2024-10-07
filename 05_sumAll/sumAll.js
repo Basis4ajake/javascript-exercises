39979658bf0756747bc07fa0bcbedb47ee3379eb
Jacob Marlowe <jacob.marlowe1991@gmail.com>
2024-10-07 15:46:37 -0400

    Remove excess checks after the initial validation confirming positive integer is entered as value

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,9 @@
 const sumAll = function(min, max){
     if (min < 0 || max < 0 || typeof min != 'number' || typeof max != 'number' || min % 1 != 0 || max % 1 != 0){
         throw e;
-    } else if ((min > max) && (min >= 0) && (max >= 0) && (min % 1 === 0) && (max % 1 === 0 && min )) {
+    } else if (min > max) {
         return totalSum = (min + max) * (min-max+1)/2;
-    } else if ((min >= 0) && (max >= 0) && (min % 1 === 0) && (max % 1 === 0)) {
+    } else {
         return totalSum = (min + max) * (max-min+1)/2;
     } 
 }
