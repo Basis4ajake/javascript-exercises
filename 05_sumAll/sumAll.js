daf349b73165e44b5e8d124ce64769f92c59c6ec
Jacob Marlowe <jacob.marlowe1991@gmail.com>
2024-10-07 15:10:43 -0400

    Check value is a whole number

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,5 @@
 const sumAll = function(min, max){
-    if (min < 0 || max < 0 || min !== Number || max !== Number) {
+    if (min < 0 || max < 0 || min !== Number || max !== Number || min % 1 !== 0 || max % 1 !== 0) {
         throw e;
     } else if (min > max) {
         return totalSum = (min + max) * (min-max+1)/2;
