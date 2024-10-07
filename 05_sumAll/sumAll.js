5539ee40e58d1c9fca1d1997f3651ccf95f3c21e
Jacob Marlowe <jacob.marlowe1991@gmail.com>
2024-10-07 12:10:07 -0400

    Starting over, not reverting to previous version though to preserve version history and thinking process

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,24 +1,3 @@
-const sumAll = function(num1, num2) {
-    let totalResult = 0;
-    if (num2 < num1) {
-        let i = num2;
-        } else {
-        i = num1;
-        }; 
-        while (i !== num1 || i !== num2) {
-        totalResult += i;
-        }
-        i++;
-        return totalResult += (num1 + num2);
-    }
-
-    
-
-    sumAll(5, 10)
-
-//Get sum of the argument numbers. Begin loop at the least of the two argument numbers
-//Increment until the counter reaches the higher number
-//Use addition assignment operator to add each count to the sum of the numbers as the new total result
 
 
 
