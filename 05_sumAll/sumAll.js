be7138001411f9b42760d713dc0b3afdb1e6518c
Jacob Marlowe <jacob.marlowe1991@gmail.com>
2024-10-07 15:51:21 -0400

    Review Solution code
    After reviewing the solution I made an adjustment
    for the cases where min is greater than the max.
    Used Array Destructuring to change the index of the
    array items instead of rearranging max and min in
    the function calculation like I had originally
    done before looking at the solution.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,11 @@
 const sumAll = function(min, max){
     if (min < 0 || max < 0 || typeof min != 'number' || typeof max != 'number' || min % 1 != 0 || max % 1 != 0){
         throw e;
-    } else if (min > max) {
-        return totalSum = (min + max) * (min-max+1)/2;
-    } else {
+    } else if (min < max) {
+        return totalSum = (min + max) * (max-min+1)/2;
+    }  
+    else if (min > max)  {
+        [min, max] = [max, min]
         return totalSum = (min + max) * (max-min+1)/2;
     } 
 }
