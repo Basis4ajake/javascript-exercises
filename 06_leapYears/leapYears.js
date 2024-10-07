14ce489f1dd5b1be8a41f80f74e1e71ac0a1ccec
Jacob Marlowe <jacob.marlowe1991@gmail.com>
2024-10-07 17:09:43 -0400

    Passed all tests
    Ran the tests and the code passed all of the tests
    on the first attempt! Lots of progress made
    since exercise 4 took several days of researching
    and experimenting to pass test 2, this one was
    completed much more quickly.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,15 +1,17 @@
 const leapYears = function(year) {
-    if (year % 4 != 0) {
+    if (year % 4 != 0) {   //Check that year is not a leap year
         let isLeapYear = false;
         return isLeapYear;
-    } else if ((year % 4 === 0) && (year % 100 === 0) && (year % 400 !== 0)) {
+    } else if ((year % 4 === 0) && (year % 100 === 0) && (year % 400 !== 0)) {  //Here we check again if the year is not a leap year
         let isLeapYear = false;
         return isLeapYear;
-    } else {
+    } else {                              //Then it must be a leap year
         let isLeapYear = true;
         return isLeapYear;
     }
 }
+
+
 
 // Do not edit below this line
 module.exports = leapYears;
