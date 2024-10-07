0ba463b5f827802b3e0acb6d174b6b7e5d58be07
Jacob Marlowe <jacob.marlowe1991@gmail.com>
2024-10-07 16:00:48 -0400

    Check year is not divisible by 100

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
 const leapYears = function(year) {
-    if (year % 4 === 0) && (year % 400 === 0) 
+    if (year % 4 === 0) && (year % 400 === 0) && (year % 100 != 0) {
+        return console.log(true);
+    }
 };
 
 // Do not edit below this line
